f108c4efa460c49833736c8abb401d0df697bf64
Partha <parthasdeyofficial@gmail.com>
2024-05-16 20:45:47 +0530

    Update : setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 
 echo -e "\e[32m[INFO] Installing the Dependencies..\e[0m"
 
-# Installing npm dependencies
 cd client
 npm i
 if [ $? -ne 0 ]; then
@@ -12,6 +11,5 @@ fi
 
 echo -e "\e[32m[INFO] Dependencies Installed!\e[0m"
 
-# Starting the Blockchain Server
 echo -e "\e[34m[INFO] Starting the Blockchain Server..\e[0m"
 ./../startup.sh
