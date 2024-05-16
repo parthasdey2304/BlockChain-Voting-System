86f609f1ef04cce33e7d6cac2cb06298ad08098c
Partha <parthasdeyofficial@gmail.com>
2024-05-16 14:30:18 +0530

    Update : setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
-echo  "\e[32m[INFO] Installing the Dependencies.."
+echo -e "\e[32m[INFO] Installing the Dependencies..\e[0m"
 cd client
 npm i
+if [ $? -ne 0 ]; then
+    echo -e "\e[31m[INFO] Failed to Install Dependencies\e[0m"
+    exit 1
+fi
 cd ..
 cd server
 pip install flask blockchain
-echo "\e[32m[INFO] Dependencies Installed!"
+if [ $? -ne 0 ]; then
+    echo -e "\e[31m[INFO] Failed to Install Dependencies\e[0m"
+    exit 1
+fi
+echo -e "\e[32m[INFO] Dependencies Installed!\e[0m"
